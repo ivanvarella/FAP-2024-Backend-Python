8cb526b3018739f57783c1862fc7e22db1918384
ivanvarella <ivanvarella@gmail.com>
2024-09-27 15:15:24 -0300

    Atividade 27-09-2024 - Material prof. tudo agora

--- Modulo3/atividades/27-09-2024/app.py
+++ Modulo3/atividades/27-09-2024/app.py
@@ -1,16 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def inicio():
-#     return "Olá pessoal com Flask"
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify
 from models import db, Cliente
 from config import Config
--- Modulo3/atividades/27-09-2024/models.py
+++ Modulo3/atividades/27-09-2024/models.py
@@ -1,22 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-
-# class Cliente(db.Model):
-#     cliente_id = db.Column(db.Integer, primary_key=True)
-#     nome = db.Column(db.String(100), nullable=False)
-#     genero = db.Column(db.String(1), nullable=False)
-#     telefone = db.Column(db.String(14), nullable=False)
-#     email = db.Column(db.String(40), nullable=False)
-
-
-# class Ficha(db.Model):
-#     ficha_id = db.Column(db.Integer, primary_key=True)
-#     data = db.Column(db.Date, nullable=False)
-#     observacao = db.Column(db.Text)
-#     cliente_id = db.Column(db.Integer, db.ForeignKey("cliente_id"), nullable=False)
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
